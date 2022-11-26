b893cdb780270a5b029225c1637b369ebf15beb9
Michael Alessi <60523720+mikealessi24@users.noreply.github.com>
2022-11-26 12:49:35 -0500

    install react query and axios

--- src/index.js
+++ src/index.js
@@ -5,13 +5,25 @@ import App from "./App"
 import { ThemeProvider } from "@mui/material/styles"
 import CssBaseline from "@mui/material/CssBaseline"
 import { theme } from "./theme"
+import { QueryClient, QueryClientProvider } from "react-query"
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      cacheTime: Infinity,
+      staleTime: Infinity,
+    },
+  },
+})
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
 root.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <CssBaseline>
-        <App />
+        <QueryClientProvider client={queryClient}>
+          <App />
+        </QueryClientProvider>
       </CssBaseline>
     </ThemeProvider>
   </React.StrictMode>
